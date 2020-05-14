6f4369c2855c18659bf8d0d549e7ff25bf7b0d02
Levente Csik <levente.b.csik@edu.bme.hu>
2020-05-14 10:16:38 +0200

    remove file

--- src/main/java/com/complexible/common/csv/provider/RowNumberProvider.java
+++
@@ -1,7 +0,0 @@
-package com.complexible.common.csv.provider;
-
-public class RowNumberProvider extends ValueProvider {
-    protected String provideValue(int rowIndex, String[] row) {
-        return String.valueOf(rowIndex);
-    }
-}
--- src/test/java/com/complexible/common/csv/CSV2RDFTest.java
+++
@@ -1,8 +0,0 @@
-package com.complexible.common.csv;
-
-/**
- * This class contains the unit tests created for the CSV2RDF class.
- */
-class CSV2RDFTest {
-
-}
